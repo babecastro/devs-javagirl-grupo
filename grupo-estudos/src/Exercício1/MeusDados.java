abe02860159ad9f68aa98c3219c7340798daba05
babecastro <barbaraellen@outlook.com.br>
2019-05-22 16:24:29 -0300

    Adiciona primeiro exercicio

---
+++ grupo-estudos/src/Exercício1/MeusDados.java
@@ -0,0 +1,15 @@
+package Exercício1;
+
+public class MeusDados {
+    public static void main(String[] args){
+        String nome = "Bárbara Ellen";
+        String idade = "24";
+        double altura = 1.68;
+        boolean gostoDeJava = true;
+
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(altura);
+        System.out.println(gostoDeJava);
+    }
+}
