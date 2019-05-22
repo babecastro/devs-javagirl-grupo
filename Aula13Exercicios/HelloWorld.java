5a0e069f296068d6814a5c425f7bd1c7f584b749
babecastro <barbaraellen@outlook.com.br>
2019-05-22 16:27:50 -0300

    Retira pastas

--- Aula13Exercicios/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package Aula13Exercicios;
-
-public class HelloWorld {
-    public static void main(String[] args){
-        String OlaMundo = "Hello World";
-        System.out.println(OlaMundo);
-    }
-}
